9a0a900c46c8c9af04693b5b0fa08b29da920488
pristit <epicnesssauce@gmail.com>
2023-02-21 16:05:25 +0200

    created file that holds all constants

---
+++ src/assets/finals/finals.ts
@@ -0,0 +1,30 @@
+export const Finals = {
+    urls: {
+        githubMain: "https://github.com/amitgoldGH",
+        linkedin: "https://www.linkedin.com/in/amit-goldshtein/",
+        manikinFront: "https://github.com/amitgoldGH/CPR-FE-BLEProject",
+        portfolio: "https://github.com/amitgoldGH/portfolio",
+        resumedl: "#",
+    },
+
+    images: {
+        afekalogo: "assets/images/afekalogo.png",
+        selalogo: "assets/images/sela-logo-black.svg",
+        manikinposter: "assets/images/manikinposter.png",
+        resumeposter: "assets/images/resume.png",
+    },
+
+    routing: {
+        title: {
+            home: "Home",
+            resume: "Resume",
+            projects: "Projects",
+        },
+
+        route: {
+            home: "home",
+            resume: "resume",
+            projects: "projects",
+        }
+    }
+}
